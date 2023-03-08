3f6c9666074bee1aff32795e59f1dc0c8772b842
dulari <dulariwathsala824@gmail.com>
2023-03-08 10:18:45 +0530

    created html and css

---
+++ main.js
@@ -0,0 +1,35 @@
+const my_name = document.getElementById("my_name");
+const button_A = documen.getElementById("button_A");
+const button_S = documen.getElementById("button_S");
+const button_D = documen.getElementById("button_D");
+const button_F = documen.getElementById("button_F");
+const button_G = documen.getElementById("button_G");
+const button_H = documen.getElementById("button_H");
+const button_J = documen.getElementById("button_J");
+const button_K = documen.getElementById("button_K");
+const button_L = documen.getElementById("button_L");
+const button_semicolon = documen.getElementById("button_semicolon");
+
+window.addEventListener("keydown",(event) => {
+    if (event.key === "A") {
+    my_name.style.willChange ="A";
+    } else if(event.key === "S") {
+                                //  ="S"; 
+    } else if(event.key === "D") {
+                                //  ="D"; 
+    } else if(event.key === "F") {
+                                //  ="F"; 
+    } else if(event.key === "G") {
+                                //  ="G"; 
+    } else if(event.key === "H") {
+                                //  ="H"; 
+    } else if(event.key === "J") {
+                                //  ="J"; 
+    } else if(event.key === "K") {
+                                //  ="K"; 
+    } else if(event.key === "L") {
+                                //  ="L"; 
+    } else if(event.key === ";") {
+                                //  =";";  
+    } 
+})
